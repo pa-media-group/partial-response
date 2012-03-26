20c6e965feb49ebe8a1e722dfe5f8efb4961faf7
Matt Nathan <matt.nathan@pressassociation.com>
2012-03-26 11:24:09 +0100

    TS-296 Update the query string builders to allow the selection of partial responses
    First step is to allow the collection of the field data as part of a query. This change adds that.
    
    The next step is to make use of the field to select the correct results

--- field-parser/src/main/java/com/pressassociation/fire/partialresponse/fields/match/Matcher.java
+++ field-parser/src/main/java/com/pressassociation/fire/partialresponse/fields/match/Matcher.java
@@ -1,5 +1,6 @@
 package com.pressassociation.fire.partialresponse.fields.match;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Predicate;
 import com.google.common.base.Splitter;
 import com.pressassociation.fire.partialresponse.fields.ast.AstNode;
@@ -15,7 +16,54 @@ import static com.google.common.base.Preconditions.checkNotNull;
  *
  * @author Matt Nathan
  */
-public class Matcher implements Predicate<CharSequence> {
+public abstract class Matcher implements Predicate<CharSequence> {
+
+  /**
+   * Holder of lazy singletons
+   */
+  @SuppressWarnings("UtilityClassWithoutPrivateConstructor")
+  private static final class Holder {
+    private static final Matcher ALL_INSTANCE = new Matcher() {
+      @Override
+      public boolean matches(CharSequence path) {
+        return true;
+      }
+
+      @Override
+      protected String patternString() {
+        return "*";
+      }
+    };
+  }
+
+  private static final class AstMatcher extends Matcher {
+    private final AstNode fields;
+
+    private AstMatcher(AstNode fields) {
+      this.fields = checkNotNull(fields);
+    }
+
+    @Override
+    public boolean matches(CharSequence input) {
+      return new MatchesPathVisitor(PATH_SPLITTER.split(input)).applyTo(fields);
+    }
+
+    @Override
+    protected String patternString() {
+      return fields.toString();
+    }
+  }
+
+  private static final Splitter PATH_SPLITTER = Splitter.on('/').omitEmptyStrings();
+
+  /**
+   * Return a matcher that will match all paths.
+   *
+   * @return The matcher.
+   */
+  public static Matcher all() {
+    return Holder.ALL_INSTANCE;
+  }
 
   /**
    * Get a new Matcher that will match against the given fields string. The string should be a valid fields string,
@@ -24,23 +72,45 @@ public class Matcher implements Predicate<CharSequence> {
    * @param fields The fields string
    * @return The matcher.
    */
-  public static Matcher valueOf(CharSequence fields) {
-    return new Matcher(new Parser().parse(checkNotNull(fields)));
+  public static Matcher of(CharSequence fields) {
+    // the default case is worth shortcutting
+    if ("*".equals(fields)) {
+      return all();
+    }
+    return new AstMatcher(new Parser().parse(checkNotNull(fields)));
   }
 
-  private static final Splitter PATH_SPLITTER = Splitter.on('/').omitEmptyStrings();
-  private final AstNode fields;
-
-  public Matcher(AstNode fields) {
-    this.fields = checkNotNull(fields);
-  }
-
-  public boolean matches(CharSequence input) {
-    return new MatchesPathVisitor(PATH_SPLITTER.split(input)).applyTo(fields);
-  }
+  public abstract boolean matches(CharSequence path);
 
   @Override
   public boolean apply(@Nullable CharSequence input) {
     return input != null && matches(input);
+  }
+
+  @Override
+  public String toString() {
+    return "Matcher[" + patternString() + ']';
+  }
+
+  protected abstract String patternString();
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof Matcher)) {
+      return false;
+    }
+    Matcher other = (Matcher) obj;
+    return other.patternString().equals(patternString());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(Matcher.class, patternString());
   }
 }
